2dc26fe37cc723265416283c23ceade8cf8c520d
bestdevmgp <pmg3858@icloud.com>
2025-01-10 09:27:18 +0900

    fix: TokenAuthenticationFilter

--- src/main/java/com/example/memoserver/global/TokenAuthenticationFilter.java
+++ src/main/java/com/example/memoserver/global/TokenAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.example.memoserver.global;
 
 import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,46 +17,40 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
-    private TokenProvider tokenProvider;
+    private final TokenProvider tokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
 
         String token = getHeaderValue(request);
         if (token != null) {
             try {
                 Authentication authentication = tokenProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-                filterChain.doFilter(request, response);
-            } catch (IllegalArgumentException e) {
-
             } catch (ExpiredJwtException e) {
-
-            } catch (SignatureException e) {
-
-            } catch (Exception e) {
-
+                setErrorResponse(response,e);
             }
-        } else {
-            throw new IllegalArgumentException("Invalid token");
         }
+
+        filterChain.doFilter(request, response); //토큰이 없거나 예외가 발생한 경우 filterChain.doFilter()가 호출되지 않아 요청이 체인의 다음 단계로 전달되지 않았
     }
 
     private String getHeaderValue(HttpServletRequest request) {
         String headerValue = request.getHeader("Authorization");
         String prefix = "Bearer ";
-
         if (headerValue != null && headerValue.startsWith(prefix)) {
             return headerValue.substring(prefix.length());
-        } else
+        } else {
             return null;
+        }
     }
 
     public void setErrorResponse(HttpServletResponse response, Throwable throwable) throws IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json;charset=utf-8");
+        response.setContentType("aplication/json;charset=utf-8");
 
         if (throwable instanceof ExpiredJwtException) {
             response.setStatus(419);
